7129e02296a641f5fc3f411b91514e9b89f5575c
Andrew <andy_htun@hotmail.com>
2015-11-26 12:38:24 +0000

    updated boris_bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,14 +2,18 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-   fail 'No bikes available' unless @bikes
+   fail 'No bikes available' if @bikes.length <= 0
     @bikes
   end
 
   def dock(bike)
-   fail 'Docking Station full' if @bikes
-    @bikes = bike
+   fail 'Docking Station full' unless @bikes.count < 20
+    @bikes << bike
   end
 
   attr_reader :bikes
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,7 +14,7 @@ describe '#release_bike' do
   it 'releases a bike' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.release_bike).to eq bike
+    expect(subject.release_bike).to eq [bike]
   end
   it 'raises an error there are no bikes available' do
     expect{subject.release_bike}.to raise_error 'No bikes available'
@@ -26,7 +26,7 @@ end
 describe '#dock' do
   it 'raise error when full' do
   subject.dock(Bike.new)
-  expect{subject.dock(Bike.new)}.to raise_error 'Docking Station full'
+  expect {20.times do subject.dock(Bike.new) end }.to raise_error 'Docking Station full'
   end
 end
 
@@ -39,13 +39,13 @@ end
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
   it 'tells which bikes are docked' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bikes).to eq bike
+    expect(subject.bikes).to eq [bike]
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
